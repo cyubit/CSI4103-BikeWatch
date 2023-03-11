faa48fd1d975a1b5fbbb781d84f1a58768930de1
M-Dyer <MDyer075@uottawa.ca>
2023-03-11 15:57:33 -0500

    Display the name of a place you click on

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -3,13 +3,26 @@ import { Map, GoogleApiWrapper, Polygon, InfoWindow } from 'google-maps-react';
 // import data from './master.json';
 import data from './master_unkeyed.json';
 
+const Name = ({name}) => (
+  <p>{name}</p>
+);
 export class MapContainer extends Component {
 
+  constructor(props){
+    super();
+    this.state={
+      name:""
+    }
+  }
+  setName=(nameValue)=>{
+    this.setState({name:nameValue});
+  }
   render() {
 
     const locations = JSON.parse(JSON.stringify(data));
 
     return (
+      <div><Name name = {this.state.name}/>
       <Map google={this.props.google}
         className={'map'}
         zoom={14}
@@ -25,12 +38,13 @@ export class MapContainer extends Component {
                 strokeOpacity={0.8}
                 strokeWeight={2}
                 fillColor="#0000FF"
-                fillOpacity={0.35}>
+                fillOpacity={0.35}
+                onClick = {()=>this.setName(item.Name)}>
               </Polygon>
             ))
           }
       </Map>
-
+      </div>
     );
   }
 }
