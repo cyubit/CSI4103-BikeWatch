b13427ec37b8b63b306290ac6e28d1a12c06b7df
AdJansen <45954995+AdJansen@users.noreply.github.com>
2023-03-11 14:34:26 -0500

    Styled the main app, added a help modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 // Removed logo since we don't need it anymore
 import MapContainer from './MapContainer.js';
+import Header from './Header.js';
 import Grid from '@mui/material/Grid';
+import './App.css';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Grid container spacing={2}>
+      <div className='App'>
+        <Grid container spacing={{xs: 2, md: 3}} style={{justifyContent:'center'}}>
           <Grid item xs={12}>
-            <h1>Bike Watch</h1>
+            <Header/>
           </Grid>
-          <Grid item xs={12} md={8} style={{position: 'relative', height: '90vh'}}>
+          {/*Change to lg={12} to lg={8} when we implement the posts component */}
+          <Grid item xs={11} lg={11}  style={{position: 'relative', height: '80vh'}}>
             <MapContainer />
           </Grid>
-          <Grid item xs={8} md={4}>
-            <h1>Why Watch</h1>
-          </Grid>
+          {/* <Grid item xs={8} lg={8}>
+            <h1>Posts Go Here!</h1>
+          </Grid> */}
         </Grid>
       </div>
     );
---
+++ src/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import helpIcon from './Help-icon.png';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const Header = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div className='Header'>
+            <Grid container spacing={2} justifyContent='space-between' alignItems="center" style={{ marginLeft: '10px' }} >
+                <Grid item xs={3}>
+                    <h1>Bike Watch</h1>
+                </Grid>
+                <Grid item xs={1}>
+                    <img src={helpIcon} alt="Help" style={{ width: "50px" }} onClick={handleOpen} />
+                </Grid>
+            </Grid>
+            <Modal open={open} onClose={handleClose}>
+                <Box sx={style}>
+                    <p>Bike Watch allows users to report bike thefts as well as view the locations and amounts of bike thefts in the Ottawa Area.</p>
+                    <p> Each area (highlighted in blue) on the map covers a neighbourhood as designated by ONS Ottawa. </p>
+                    <p>Hover over one to see police data for that area. Click on the "Report Theft" button to report a theft (User reporting is currently in development, but will be available soon). </p>
+                    <p>Click anywhere off of this box to return to the app.</p>
+                </Box>
+            </Modal>
+        </div>
+
+    );
+}
+
+export default Header;
